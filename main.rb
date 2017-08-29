d494699fb5ed5bd03885fba24b179d93d0164402
Gary Ravago <ravagog@gmail.com>
2017-08-28 19:44:12 -0700

    Initial commit

---
+++ main.rb
@@ -0,0 +1,8 @@
+require_relative 'linked_list_node'
+require_relative 'stack'
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
---
+++ stack.rb
@@ -0,0 +1,19 @@
+class Stack
+  attr_reader :data
+
+  def initialize
+      @data = nil
+  end
+
+  # Push a value onto the stack
+  def push(value)
+      
+  end
+
+  # Pop an item off the stack.
+  # Remove the last item that was pushed onto the
+  # stack and return the value to the user
+  def pop
+      
+  end
+end
